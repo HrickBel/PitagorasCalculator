bac8ec7eb4b381242f3bf5678fd85011f1ef3d38
HrickBel <115942716+HrickBel@users.noreply.github.com>
2022-10-24 15:05:06 -0300

    Add files via upload

---
+++ TeoremaDePitagoras.js
@@ -0,0 +1,26 @@
+﻿var c = document.getElementById("canva");
+var p = c.getContext("2d");
+p.lineWidth = 5;
+p.moveTo(55, 55);
+p.lineTo(55, 250);
+p.moveTo(55, 250);
+p.lineTo(700, 250);
+p.lineTo(55, 55);
+p.stroke();
+
+function pitagoras() {
+    var result;
+    var a = document.getElementsByName("catA")[0].value;
+    var b = document.getElementsByName("catB")[0].value;
+    var h = document.getElementsByName("hip")[0].value;
+    if (Number(a) == "") {
+        result = Math.sqrt(Math.pow(parseFloat(h), 2) - Math.pow(parseFloat(b), 2));
+        document.getElementsByName("catA")[0].value = result;
+    } else if (Number(b) == "") {
+        result = Math.sqrt(Math.pow(parseFloat(h), 2) - Math.pow(parseFloat(a), 2));
+        document.getElementsByName("catB")[0].value = result;
+    } else {
+        result = Math.sqrt(Math.pow(parseFloat(a), 2) + Math.pow(parseFloat(b), 2));
+        document.getElementsByName("hip")[0].value = result;
+    }
+}
